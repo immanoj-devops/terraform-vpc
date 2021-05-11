1a02bfb5ab7efb120a962287ac6a2c257c48eb1b
Manoj Pachigolla <mp7652@zebra.com>
2021-05-11 04:50:03 -0500

    --- Auto-Git Commit ---

--- .history/route-table_20210511044957.tf
+++ .history/route-table_20210511044957.tf
@@ -20,7 +20,6 @@ resource "aws_route_table" "prv-rt" {
     cidr_block = "10.0.1.0/24"
     gateway_id = aws_internet_gateway.example.id
   }
-
   tags = {
     Name = "Name = "${var.PROJECT_NAME}-PrvRT""
   }
