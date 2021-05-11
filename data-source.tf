6a0af7633b110d4c7a3e764876402f5d19b574e6
Manoj Pachigolla <mp7652@zebra.com>
2021-05-11 05:30:08 -0500

    --- Auto-Git Commit ---

--- data-source.tf
+++
@@ -1,9 +0,0 @@
-data "aws_vpc" "vpcid" {
-  id = var.DEFAULD_VPC_ID
-}
-
-resource "aws_subnet" "example" {
-  vpc_id            = data.aws_vpc.selected.id
-  availability_zone = "us-west-2a"
-  cidr_block        = cidrsubnet(data.aws_vpc.selected.cidr_block, 4, 1)
-}
